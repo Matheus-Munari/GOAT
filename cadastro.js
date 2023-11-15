06bd9809640685645284d01224098ff18b80207d
MatheusMunari <matheus.denani@sptech.school>
2023-11-15 12:05:54 -0300

    Adicionei as funcionalidades de mudar o formulario na pagina de login/cadastro

---
+++ cadastro.js
@@ -0,0 +1,11 @@
+function validarInformacoesUsuario() {
+    var nomeCadastro = i_cadastro_nome.value;
+    var nascimentoCadastro = i_cadastro_nascimento.value;
+    var emailCadastro = i_cadastro_email.value;
+    var senhaCadastro = i_cadastro_senha.value;
+    var confirmacaoSenhaCadastro = i_cadastro_confirmacao_senha.value;
+
+   console.log("Oi")
+
+   cadastroEndereco();
+}
