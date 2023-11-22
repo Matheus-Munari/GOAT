ae9d8f145f94708a74d87f4b0d56664208361c02
Matheus Munari <matheus.denani@sptech.school>
2023-11-22 14:08:24 -0300

    Adicionei a funcionalidade do grafico da dashboard

--- web-data-viz/site/src/models/jogadorModel.js
+++ web-data-viz/site/src/models/jogadorModel.js
@@ -2,7 +2,7 @@ var database = require("../database/config")
 
 function listar() {
     var instrucao = `
-        SELECT * FROM estatisticasTemporada;
+        SELECT * FROM estatisticasTemporada ORDER BY fkTemporada;
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
